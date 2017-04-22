437e85233150fc9f18a37761c4c9ddfb36d14730
Or Neeman <oneeman@gmail.com>
2017-04-21 21:28:26 -0600

    Finish misanthropy page

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,15 +1,27 @@
 var webpack = require("webpack");
 
+function isExternal(module) {
+  var userRequest = module.userRequest;
+
+  if (typeof userRequest !== 'string') {
+    return false;
+  }
+
+  return userRequest.indexOf('bower_components') >= 0 ||
+    userRequest.indexOf('node_modules') >= 0 ||
+    userRequest.indexOf('libraries') >= 0;
+}
+
 const config = {
   entry: {
     misanthropy: './src/misanthropy.jsx',
-    vendor: ["react", "lodash"],
   },
 
   output: {
     filename: '[name].js',
     path: __dirname + "/..",
   },
+  externals: {vendor: true},
   module: {
     rules: [
       {
@@ -22,10 +34,20 @@ const config = {
       },
     ],
   },
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
+
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({name: "vendor", filename: "vendor.js"}),
-  ],
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function(module) {
+        return isExternal(module);
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      mangle: true
+    }),
+  ]
 };
 
 module.exports = config;
